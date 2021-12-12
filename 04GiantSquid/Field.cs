b01b742fbc8bbc53a7f6fdb042c25a4937f8f966
tammowhs <77109700+tammowhs@users.noreply.github.com>
2021-12-12 18:59:14 +0100

    evaluate nr 1

---
+++ 04GiantSquid/Field.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._04GiantSquid
+{
+    public class Field
+    {
+        public int Value { get; }
+        public int Row { get; }
+        public int Col { get; }
+        public int Block { get; }
+        public bool Marked { get; set; }
+
+        public Field(int value, int row, int col, int block)
+        {
+            Value = value;
+            Row = row;
+            Col = col;
+            Block = block;
+            Marked = false;
+        }
+    }
+}
--- 04GiantSquid/GiantSquid.cs
+++ 04GiantSquid/GiantSquid.cs
@@ -9,57 +9,129 @@ namespace AdventOfCode._04GiantSquid
 	public class GiantSquid : ICodePuzzle
 	{
 		public int EvaluatePartOne(string input)
-		{
-			input = @"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+        {
+            input = @"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
-22 13 17 11  0
- 8  2 23  4 24
-21  9 14 16  7
- 6 10  3 18  5
- 1 12 20 15 19
+            22 13 17 11  0
+             8  2 23  4 24
+            21  9 14 16  7
+             6 10  3 18  5
+             1 12 20 15 19
 
- 3 15  0  2 22
- 9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
+             3 15  0  2 22
+             9 18 13 17  5
+            19  8  7 25 23
+            20 11 10 24  4
+            14 21 16 12  6
 
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7";
+            14 21 17 24  4
+            10 16 15  9 19
+            18  8 23 26 20
+            22 11 13  6  5
+             2  0 12  3  7";
 
-			var inputBlocks = input.Split("\r\n\r\n").ToList();
+            List<int> drawnNumbers;
+            List<List<Field>> bingoBlocks;
+            DeserializeInput(input, out drawnNumbers, out bingoBlocks);
 
-			var drawnNumbers = inputBlocks[0].Split(',').Select(numString => int.Parse(numString)).ToList();
+            foreach (var drawnNumber in drawnNumbers)
+            {
+                foreach (var bingoBlock in bingoBlocks)
+                {
+                    var field = bingoBlock.Find(f => f.Value == drawnNumber);
 
-			inputBlocks.RemoveAt(0);
+                    if (field is not null)
+                    {
+                        field.Marked = true;
 
-			var bingoFields = inputBlocks.Select(iB => DeserializeBingoField(iB).ToList()).ToList();
+                        if (CheckBingoBlockForWin(bingoBlock))
+                        {
+                            var sumOfUnmarked = bingoBlock
+                                .Where(f => !f.Marked)
+                                .Sum(f => f.Value);
 
-			return 0;
+                            var bingoScore = sumOfUnmarked * drawnNumber;
+
+                            return bingoScore;
+                        }
+                    }
+                }
+            }
+
+            throw new Exception("No Bingo");
+        }
+
+        private void DeserializeInput(string input, out List<int> drawnNumbers, out List<List<Field>> bingoFields)
+        {
+            var inputBlocks = input.Split("\r\n\r\n").ToList();
+
+            drawnNumbers = inputBlocks[0].Split(',').Select(numString => int.Parse(numString)).ToList();
+            inputBlocks.RemoveAt(0);
+
+            bingoFields = inputBlocks.Select((iBString, blockIndex) => DeserializeBingoBlock(iBString, blockIndex)).ToList();
+        }
+
+        private bool CheckBingoBlockForWin(List<Field> bingoField)
+        {
+			var groupedByRows = bingoField.GroupBy(f => f.Row).ToList();
+
+			var anyRowWin = groupedByRows.Any(group => group.All(f => f.Marked));
+
+			var groupedByCols = bingoField.GroupBy(f => f.Col).ToList();
+
+			var anyColWin = groupedByCols.Any(group => group.All(f => f.Marked));
+
+			var anyWin = anyRowWin || anyColWin;
+			
+			return anyWin;
 		}
 
-		public IEnumerable<IEnumerable<int>> DeserializeBingoField(string blockInput)
+		public List<Field> DeserializeBingoBlock(string blockInput, int blockIndex)
 		{
 			var rowStrings = blockInput.Split("\r\n").ToList();
 
-			foreach (var rowString in rowStrings)
-			{
-				var rowValues = rowString
-					.Split(' ')
-					.Where(str => !string.IsNullOrWhiteSpace(str))
-					.Select(numString => int.Parse(numString))
-					.ToList();
+			var blockFields = rowStrings
+				.SelectMany((rowString, rowIndex) => rowString
+														.Split(' ')
+														.Where(str => !string.IsNullOrWhiteSpace(str))
+														.Select((numString, colIndex) => new Field(int.Parse(numString), rowIndex, colIndex, blockIndex))
+				)
+				.ToList();
 
-				yield return rowValues;
-			}
+			return blockFields;
 		}
 
 		public int EvaluatePartTwo(string input)
 		{
-			throw new NotImplementedException();
+            List<int> drawnNumbers;
+            List<List<Field>> bingoBlocks;
+            DeserializeInput(input, out drawnNumbers, out bingoBlocks);
+
+            foreach (var drawnNumber in drawnNumbers)
+            {
+                foreach (var bingoBlock in bingoBlocks)
+                {
+                    var field = bingoBlock.Find(f => f.Value == drawnNumber);
+
+                    if (field is not null)
+                    {
+                        field.Marked = true;
+
+                        if (CheckBingoBlockForWin(bingoBlock))
+                        {
+                            var sumOfUnmarked = bingoBlock
+                                .Where(f => !f.Marked)
+                                .Sum(f => f.Value);
+
+                            var bingoScore = sumOfUnmarked * drawnNumber;
+
+                            return bingoScore;
+                        }
+                    }
+                }
+            }
+
+            return 0;
 		}
 	}
 }
