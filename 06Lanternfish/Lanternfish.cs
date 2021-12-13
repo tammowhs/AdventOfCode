fa2e9f3102f3556f8951ca4add0202ca58ef1635
tammowhs <77109700+tammowhs@users.noreply.github.com>
2021-12-13 13:09:14 +0100

    Update Lanternfish part 2

--- 06Lanternfish/Lanternfish.cs
+++ 06Lanternfish/Lanternfish.cs
@@ -14,7 +14,7 @@ namespace AdventOfCode._06Lanternfish
 
             var fishes = input.Split(',').Select(t => new Fish(int.Parse(t))).ToList();
 
-            for (int i = 0; i < 256; i++)
+            for (int i = 0; i < 80; i++)
             {
                 var breedingFishes = fishes.Where(f => f.Timer == 0).ToList();
 
@@ -32,27 +32,78 @@ namespace AdventOfCode._06Lanternfish
 
         public int EvaluatePartTwo(string input)
         {
-            throw new NotImplementedException();
             //input = @"3,4,3,1,2";
 
-            //var fishes = input.Split(',').Select(t => int.Parse(t));
+            var initialFishes = input.Split(',').Select(t => int.Parse(t)).ToArray();
 
-            //for (int i = 0; i < 80; i++)
-            //{
-            //    //Console.WriteLine(i);
-            //    fishes = fishes.Select(f => f - 1);
+            var days = 256;
 
-            //    //Console.WriteLine("2: ", i); 
-            //    var breedingFishes = fishes.Where(f => f < 0).Select(f => f = 6);
+            var newBirths = new long[days];
 
-            //    //Console.WriteLine("3: ", i);
-            //    var newFishes = breedingFishes.Select(e => 8);
+            foreach (var fish in initialFishes)
+            {
+                var birthdays = GetBirthDays(fish).Where(x => x < days);
 
-            //    //Console.WriteLine("4: ", i);
-            //    fishes = fishes.Concat(newFishes);
-            //}
+                foreach (var birthDay in birthdays)
+                {
+                    newBirths[birthDay]++;
+                }
+            }
 
-            //return fishes.ToList().Count;
+            for (int day = 1; day <= days; day++)
+            {
+                var newBorn = newBirths[day - 1];
+
+                var birthdays = GetBirthDays(8).Where(d => d < days - day);
+
+                foreach (int birthDay in birthdays)
+                {
+                    newBirths[day + birthDay] += newBorn;
+                }
+
+            }
+
+            var y = newBirths.Sum() + initialFishes.Length;
+
+            var x = PopulationAfter(days, initialFishes);
+
+            return 0;
+        }
+
+        //a.e. fish with initial age 3 will give birth on day 4 (index 3), day 11 (i 10) and so on
+        private IEnumerable<int> GetBirthDays(int initialAge)
+        {
+            return Enumerable.Range(0, 40)
+                .Select(x => x * 7 + initialAge);
+        }
+
+        long PopulationAfter(int days, int[] initial)
+        {
+            long[] newbirths = new long[days];
+
+            IEnumerable<int> births(int initialAge, int daysLeft) => Enumerable.Range(0, 50)
+                .Select(x => x * 7 + initialAge)
+                .Where(x => x < daysLeft);
+
+            foreach (var age in initial)
+            {
+                foreach (var birthday in births(age, days))
+                {
+                    newbirths[birthday] += 1;
+                }
+            }
+
+            for (int day = 0; day < days; day++)
+            {
+                var newBorn = newbirths[day];
+
+                foreach (var birthday in births(8, days - day - 1))
+                {
+                    newbirths[day + 1 + birthday] += newBorn;
+                }
+            }
+
+            return initial.Length + newbirths.Sum();
         }
     }
 }
