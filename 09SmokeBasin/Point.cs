201516f3243bce88992a25036fe515494ebbb4dc
tammowhs <77109700+tammowhs@users.noreply.github.com>
2021-12-18 11:41:19 +0100

    09 part 1

---
+++ 09SmokeBasin/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._09SmokeBasin
+{
+	public class Point
+	{
+		private readonly int x;
+		private readonly int y;
+		private readonly int height;
+		private bool isLowest = false;
+
+		public int X => x;
+		public int Y => y;
+		public int Height => height;
+		public bool IsLowest { get => isLowest; set => isLowest = value; }
+
+		public Point(int x, int y, int height)
+		{
+			this.x = x;
+			this.y = y;
+			this.height = height;
+		}
+	}
+}
---
+++ 09SmokeBasin/SmokeBasin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._09SmokeBasin
+{
+	public class SmokeBasin : ICodePuzzle
+	{
+		private readonly string testInput = @"2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+
+		public int EvaluatePartOne(string input)
+		{
+			//input = testInput;
+
+			var points = input
+				.Split("\r\n")
+				.Select((rowString, yIndex) =>
+					rowString
+						.ToCharArray()
+						.Select((heightChar, xIndex) =>
+							new Point(xIndex, yIndex, int.Parse(heightChar.ToString()))
+						)
+				).SelectMany(r => r)
+				.ToList();
+
+            foreach (var point in points)
+            {
+				var adjacents = FindAdjacents(points, point).ToList();
+
+				if(adjacents.All(adjacents => adjacents.Height > point.Height))
+                {
+					point.IsLowest = true;
+                }
+            }
+
+			var allLowest = points.Where(p => p.IsLowest);
+
+			var heights = allLowest.Select(p => p.Height);
+
+			var sumHeights = heights.Sum();
+
+			var solution = sumHeights + allLowest.Count();
+
+			return solution;
+		}
+
+		private IEnumerable<Point> FindAdjacents(IEnumerable<Point> points, Point point)
+        {
+			return points.Where(p => (p.Y == point.Y && p.X >= point.X - 1 && p.X <= point.X + 1)
+								  || (p.X == point.X && p.Y >= point.Y - 1 && p.Y <= point.Y + 1))
+						.Where(p => !(p.X == point.X && p.Y == point.Y));
+        }
+
+		public int EvaluatePartTwo(string input)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
